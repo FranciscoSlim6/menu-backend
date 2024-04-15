7933dd9c91aa7757a2887dee876f23ea0425b83d
Francisco Delgado Martinez <francisco.slim16@gmail.com>
2024-04-15 00:00:04 -0500

    se elimina el validatePipes de main

--- src/main.ts
+++ src/main.ts
@@ -6,14 +6,6 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  app.useGlobalPipes(
-    new ValidationPipe({
-      whitelist: true,
-      forbidNonWhitelisted: true,
-      transform: true,
-    }),
-  );
-
   await app.enableCors();
 
   const swaggerOptions  = new DocumentBuilder()
